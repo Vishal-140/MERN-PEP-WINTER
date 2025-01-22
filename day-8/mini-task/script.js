5756817b352686c8c0342e1c12c66f773371b538
Vishal Kumar Chaurasia <vkc140@gmail.com>
2025-01-22 21:24:50 +0530

    mini task

---
+++ day-8/mini-task/script.js
@@ -0,0 +1,52 @@
+const events = [
+    {
+        title: "Welcome Week",
+        description: "Kick off the year with fun activities and events for new students.",
+    },
+    {
+        title: "Club Fair",
+        description: "Explore different student clubs and organizations on campus.",
+    },
+    {
+        title: "Career Fair",
+        description: "Network with potential employers and learn about career opportunities.",
+    },
+    {
+        title: "Homecoming Game",
+        description: "Cheer on the school's football team at the annual homecoming game.",
+    },
+    {
+        title: "Fall Festival",
+        description: "Enjoy seasonal festivities, food, and music at the fall festival.",
+    },
+    {
+        title: "Guest Speaker Series",
+        description: "Hear from influential figures in various fields.",
+    },
+    {
+        title: "Study Abroad Fair",
+        description: "Learn about study abroad programs and opportunities.",
+    },
+    {
+        title: "Volunteer Day",
+        description: "Give back to the community by participating in a volunteer event.",
+    },
+    {
+        title: "Spring Concert",
+        description: "Enjoy live music performances by student and professional artists.",
+    },
+];
+
+
+
+const cards = document.querySelectorAll('.card')
+
+events.forEach((e, index) => {
+    if(index < cards.length) {
+        const title = cards[index].querySelector('h2');
+        const desc = cards[index].querySelector('p');
+        title.textContent = e.title;
+        desc.textContent = e.description;
+    }
+});
+
