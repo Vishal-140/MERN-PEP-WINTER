298e26a3634294d59b47c579faeafe156c6247e0
Vishal Kumar Chaurasia <vkc140@gmail.com>
2025-01-23 23:50:15 +0530

    day-9

--- day-9/events.js
+++ day-9/events.js
@@ -1,12 +1,11 @@
 const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
 
     const selectInput = e.target[3];
     const selectedValue = selectInput.value;
 
     if (selectedValue === "NA") {
-        alert("Please select your PROFESSION");
+        alert("Please select your profession.");
     } else {
         window.open("./thank.html", "_self");
     }
@@ -18,28 +17,7 @@ const handleSelectProfession = (e) => {
     const empEmailInput = document.querySelector('input[name="emp-email"]');
     const gstNumberInput = document.querySelector('input[name="gst-number"]');
 
-    switch (selectedValue) {
-        case "NA": {
-            alert("Please select your PROFESSION");
-            break;
-        }
-        case "std": {
-            collegeNameInput.disabled = false;
-            empEmailInput.disabled = true;
-            gstNumberInput.disabled = true;
-            break;
-        }
-        case "emp": {
-            collegeNameInput.disabled = true;
-            empEmailInput.disabled = false;
-            gstNumberInput.disabled = true;
-            break;
-        }
-        case "bsn": {
-            collegeNameInput.disabled = true;
-            empEmailInput.disabled = true;
-            gstNumberInput.disabled = false;
-            break;
-        }
-    }
+    collegeNameInput.disabled = selectedValue !== "std";
+    empEmailInput.disabled = selectedValue !== "emp";
+    gstNumberInput.disabled = selectedValue !== "bsn";
 };
